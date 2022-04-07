124a785d10f073a18d1f7c2b916940be8c99dbcb
Lex Davis <lexdavis@Lexs-MBP.attlocal.net>
2022-04-06 20:44:35 -0500

    this is my text based game project

---
+++ game.js
@@ -0,0 +1,195 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('btn')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
+
+const textNodes = [
+  {
+    id: 1,
+    text: 'You wake up in a strange place and you see a full jar of slime near you.',
+    options: [
+      {
+        text: 'Take the slime',
+        setState: { slime: true },
+        nextText: 2
+      },
+      {
+        text: 'Leave the slime',
+        nextText: 2
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'You venture forth in search of answers to where you are when you come across a alien.',
+    options: [
+      {
+        text: 'Trade the slime for a sword',
+        requiredState: (currentState) => currentState.slime,
+        setState: { slime: false, sword: true },
+        nextText: 3
+      },
+      {
+        text: 'Trade the slime for a shield',
+        requiredState: (currentState) => currentState.slime,
+        setState: { slime: false, shield: true },
+        nextText: 3
+      },
+      {
+        text: 'Ignore the alien',
+        nextText: 3
+      }
+    ]
+  },
+  {
+    id: 3,
+    text: 'After leaving the alien you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+    options: [
+      {
+        text: 'Explore the castle',
+        nextText: 4
+      },
+      {
+        text: 'Find a room to sleep at in the town',
+        nextText: 5
+      },
+      {
+        text: 'Find a room full of hay in a stable to sleep in',
+        nextText: 6
+      }
+    ]
+  },
+  {
+    id: 4,
+    text: 'You are so tired that you fall asleep while exploring the castle and are killed by some terrible alien monster in your sleep.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 5,
+    text: 'Without any money to buy a room you break into the nearest hotel and fall asleep. After a few hours of sleep the owner of the hotel finds you and has the town guard lock you in a cell.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 6,
+    text: 'You wake up well rested and full of energy ready to explore the nearby castle.',
+    options: [
+      {
+        text: 'Explore the castle',
+        nextText: 7
+      }
+    ]
+  },
+  {
+    id: 7,
+    text: 'While exploring the castle you come across a horrible alien monster in your path.',
+    options: [
+      {
+        text: 'Try to run',
+        nextText: 8
+      },
+      {
+        text: 'Attack it with your sword',
+        requiredState: (currentState) => currentState.sword,
+        nextText: 9
+      },
+      {
+        text: 'Hide behind your shield',
+        requiredState: (currentState) => currentState.shield,
+        nextText: 10
+      },
+      {
+        text: 'Throw the slime at it',
+        requiredState: (currentState) => currentState.slime,
+        nextText: 11
+      }
+    ]
+  },
+  {
+    id: 8,
+    text: 'Your attempts to run are in vain and the alien monster easily catches.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 9,
+    text: 'You foolishly thought this alien monster could be slain with a single sword.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 10,
+    text: 'The alien monster laughed as you hid behind your shield and ate you to pieces.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 11,
+    text: 'You threw your jar of slime at the alien monster and it exploded. After the dust settled you saw the alien monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+    options: [
+      {
+        text: 'Congratulations. Play Again.',
+        nextText: -1
+      }
+    ]
+  }
+]
+
+startGame()
